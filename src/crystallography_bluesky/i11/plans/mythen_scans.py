f7a0c0f7243f5b82b55b054438c22dc1a1aec6dd
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-04 11:50:58 +0000

    removed some comments

--- src/crystallography_bluesky/i11/plans/mythen_scans.py
+++ src/crystallography_bluesky/i11/plans/mythen_scans.py
@@ -6,20 +6,18 @@ import bluesky.preprocessors as bpp
 import numpy as np
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
-
-# from dodal.devices.i11.diff_stages import DiffractometerStage
 from dodal.devices.i11.mythen import Mythen3
 from dodal.devices.motors import Motor
 from dodal.log import LOGGER
 from dodal.plan_stubs.data_session import attach_data_session_metadata_decorator
 from dodal.utils import get_beamline_name
-from ophyd_async.core import TriggerInfo  # ,DEFAULT_TIMEOUT
+from ophyd_async.core import DEFAULT_TIMEOUT, TriggerInfo
 from pydantic import NonNegativeFloat, NonNegativeInt, validate_call
 
 BL = get_beamline_name(os.environ("BEAMLINE"))  # type: ignore
 
 DEFAULT_MYTHEN: Mythen3 = inject("mythen3")
-DEFAULT_STAGE: Motor = inject("diff_stage.delta")
+DEFAULT_AXIS: Motor = inject("diff_stage.delta")
 
 
 def create_steps(
@@ -46,7 +44,7 @@ def mythen_scan(
     step: float | int | None = None,
     duration: NonNegativeFloat | NonNegativeInt = 1.0,
     mythen: Mythen3 = DEFAULT_MYTHEN,
-    axis: Motor = DEFAULT_STAGE,
+    axis: Motor = DEFAULT_AXIS,
 ) -> MsgGenerator:
     """
     Simple software triggered count plan for the mythen3 on i11
@@ -68,7 +66,7 @@ def mythen_scan(
 
     trigger_info = TriggerInfo(number_of_events=number_of_images, livetime=duration)
     yield from bps.prepare(mythen, trigger_info, group="setup")
-    yield from bps.wait(group="setup")
+    yield from bps.wait(group="setup", timeout=DEFAULT_TIMEOUT)
 
     yield from bsp.list_scan(
         [mythen], (axis, step_points)
