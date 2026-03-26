12ed6fad679f726acd376e539c1b5c8a78036b1c
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-03-26 11:18:44 +0000

    check hutch status before robot load or  unload

--- src/crystallography_bluesky/i15_1/plans/robot.py
+++ src/crystallography_bluesky/i15_1/plans/robot.py
@@ -6,14 +6,30 @@ from dodal.devices.beamlines.i15_1.robot import (
     Robot,
     SampleLocation,
 )
+from dodal.devices.hutch_shutter import HutchInterlock
+
+robot = inject("robot")
+hutch_interlock = inject("hutch_interlock")
 
 
 def robot_load(
-    puck: int, position: int, robot: Robot = inject("robot")
+    puck: int,
+    position: int,
+    robot: Robot = robot,
+    hutch_interlock: HutchInterlock = hutch_interlock,
 ) -> MsgGenerator[None]:
+    hutch_status = yield from bps.rd(hutch_interlock.status)
+    assert hutch_status == 0, f"Hutch status was not 0, but instead {hutch_status}"
+
     sample = SampleLocation(puck, position)
     yield from bps.abs_set(robot, sample, wait=True)
 
 
-def robot_unload(robot: Robot = inject("robot")) -> MsgGenerator[None]:
+def robot_unload(
+    robot: Robot = robot,
+    hutch_interlock: HutchInterlock = hutch_interlock,
+) -> MsgGenerator[None]:
+    hutch_status = yield from bps.rd(hutch_interlock.status)
+    assert hutch_status == 0, f"Hutch status was not 0, but instead {hutch_status}"
+
     yield from bps.abs_set(robot, SAMPLE_LOCATION_EMPTY, wait=True)
