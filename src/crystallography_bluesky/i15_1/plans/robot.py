9ce03fb48e1522b9ed402789c089dcbd627125b7
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-04-15 11:52:30 +0000

    add test

--- src/crystallography_bluesky/i15_1/plans/robot.py
+++ src/crystallography_bluesky/i15_1/plans/robot.py
@@ -1,6 +1,7 @@
 from bluesky import plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
+from dodal.devices.beamlines.i15_1.gonio_interlock import GonioInterlock
 from dodal.devices.beamlines.i15_1.robot import (
     SAMPLE_LOCATION_EMPTY,
     Robot,
@@ -10,8 +11,10 @@ from dodal.devices.hutch_shutter import HutchInterlock
 
 robot = inject("robot")
 hutch_interlock = inject("hutch_interlock")
+gonio_interlock = inject("gonio_interlock")
 
 HUTCH_SAFE_FOR_OPERATIONS = 0
+GONIO_SAFE_FOR_OPERATIONS = 65535  # All 16 bits are true
 
 
 def robot_load(
@@ -19,10 +22,17 @@ def robot_load(
     position: int,
     robot: Robot = robot,
     hutch_interlock: HutchInterlock = hutch_interlock,
+    gonio_interlock: GonioInterlock = gonio_interlock,
 ) -> MsgGenerator[None]:
     hutch_status = yield from bps.rd(hutch_interlock.status)
     assert hutch_status == HUTCH_SAFE_FOR_OPERATIONS, (
-        f"Hutch status was not 0, but instead {hutch_status}."
+        f"Hutch status was not {HUTCH_SAFE_FOR_OPERATIONS}, but instead {hutch_status}."
+    )
+
+    gonio_status = yield from bps.rd(gonio_interlock.status)
+    assert gonio_status == GONIO_SAFE_FOR_OPERATIONS, (
+        f"Goniometer interlock was not {GONIO_SAFE_FOR_OPERATIONS}, \
+            but instead {gonio_status}"
     )
 
     sample = SampleLocation(puck, position)
--- tests/unit_tests/i15_1/test_robot.py
+++ tests/unit_tests/i15_1/test_robot.py
@@ -1,5 +1,6 @@
 import pytest
 from bluesky.run_engine import RunEngine
+from dodal.devices.beamlines.i15_1.gonio_interlock import GonioInterlock
 from dodal.devices.beamlines.i15_1.robot import Robot
 from dodal.devices.hutch_shutter import HutchInterlock
 from ophyd_async.core import init_devices, set_mock_value
@@ -20,6 +21,13 @@ async def hutch_interlock() -> HutchInterlock:
     async with init_devices(mock=True):
         hutch_interlock = HutchInterlock("", "")
     return hutch_interlock
+
+
+@pytest.fixture
+async def gonio_interlock() -> GonioInterlock:
+    async with init_devices(mock=True):
+        gonio_interlock = GonioInterlock("", "")
+    return gonio_interlock
 
 
 async def test_plan_loads_robot(robot: Robot, hutch_interlock: HutchInterlock):
@@ -52,6 +60,26 @@ async def test_correct_error_is_raised_when_hutch_is_not_safe_to_operate(
     robot: Robot, hutch_interlock: HutchInterlock, status: int, reason: str
 ):
     set_mock_value(hutch_interlock.status, status)
+    RE = RunEngine()
+    with pytest.raises(AssertionError, match=reason):
+        RE(robot_load(1, 2, robot, hutch_interlock))
+
+
+@pytest.mark.parametrize(
+    "status, reason",
+    (
+        [65535, "Hutch status was not 0, but instead 2."],
+        [65535, "Hutch status was not 0, but instead 7."],
+    ),
+)
+async def test_correct_error_is_raised_when_gonio_is_not_safe_to_operate(
+    robot: Robot,
+    hutch_interlock: HutchInterlock,
+    gonio_interlock: GonioInterlock,
+    status: float,
+    reason: str,
+):
+    set_mock_value(gonio_interlock.status, 65535)
     RE = RunEngine()
     with pytest.raises(AssertionError, match=reason):
         RE(robot_load(1, 2, robot, hutch_interlock))
