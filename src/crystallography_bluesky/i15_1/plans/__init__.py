583b8e216856264b0f1f7707bde618de7f0dad09
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-04-30 13:26:42 +0100

    Small fixes

--- src/crystallography_bluesky/i15_1/plans/__init__.py
+++ src/crystallography_bluesky/i15_1/plans/__init__.py
@@ -1,3 +1,4 @@
 from .robot import robot_load, robot_unload
+from .snapshots import take_snapshot
 
-__all__ = ["robot_load", "robot_unload"]
+__all__ = ["robot_load", "robot_unload", "take_snapshot"]
--- src/crystallography_bluesky/i15_1/plans/snapshots.py
+++ src/crystallography_bluesky/i15_1/plans/snapshots.py
@@ -1,13 +1,13 @@
 import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+from bluesky.utils import MsgGenerator
 from ophyd_async.core import TriggerInfo
 from ophyd_async.epics.adcore import ContAcqDetector
 
 
-# @bpp.run_decorator()
-def take_snapshot(camera: ContAcqDetector):
-    yield from bps.open_run()
+@bpp.run_decorator()
+def take_snapshot(camera: ContAcqDetector) -> MsgGenerator:
     yield from bps.stage(camera)
     yield from bps.prepare(camera, TriggerInfo(number_of_events=1), wait=True)
     yield from bps.trigger_and_read([camera])
     yield from bps.unstage(camera)
-    yield from bps.close_run()
