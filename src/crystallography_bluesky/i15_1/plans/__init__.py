416cfb35e28a2a66d33c6c8ceeeda43aa2a4ba76
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-05-18 09:40:26 +0100

    Fix

--- src/crystallography_bluesky/i15_1/plans/__init__.py
+++ src/crystallography_bluesky/i15_1/plans/__init__.py
@@ -1,15 +1,15 @@
-# from .robot import (
-#     move_hexapod_to_home_position,
-#     prepare_beamline_for_robot_load,
-#     robot_load,
-#     robot_unload,
-# )
+from .robot import (
+    move_hexapod_to_home_position,
+    prepare_beamline_for_robot_load,
+    robot_load,
+    robot_unload,
+)
 from .take_eiger_data import take_eiger_data
 
 __all__ = [
-    # "robot_load",
-    # "move_hexapod_to_home_position",
-    # "prepare_beamline_for_robot_load",
-    # "robot_unload",
+    "robot_load",
+    "move_hexapod_to_home_position",
+    "prepare_beamline_for_robot_load",
+    "robot_unload",
     "take_eiger_data",
 ]
