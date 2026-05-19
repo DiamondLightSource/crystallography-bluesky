d6bfb25b12a38d5d6694b1e8ae211d1da5a240ab
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-05-19 13:33:08 +0100

    Fix

--- src/crystallography_bluesky/i15_1/plans/__init__.py
+++ src/crystallography_bluesky/i15_1/plans/__init__.py
@@ -1,10 +1,16 @@
-from .robot import move_hexapod_to_home_position, robot_load, robot_unload
+from .robot import (
+    move_hexapod_to_home_position,
+    prepare_beamline_for_robot_load,
+    robot_load,
+    robot_unload,
+)
 from .snapshots import take_snapshot
 from .take_i0_data import take_i0_data
 
 __all__ = [
-    "move_hexapod_to_home_position",
     "robot_load",
+    "move_hexapod_to_home_position",
+    "prepare_beamline_for_robot_load",
     "robot_unload",
     "take_i0_data",
     "take_snapshot",
