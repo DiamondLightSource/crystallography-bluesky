8a7a44f46c1db769b82c064f7d163e5d5418d723
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-05-20 15:30:39 +0100

    Add test

--- tests/unit_tests/i15_1/test_take_eiger_data.py
+++ tests/unit_tests/i15_1/test_take_eiger_data.py
@@ -80,3 +80,105 @@ def test_take_eiger_data_makes_expected_calls(eiger: EigerDetector):
             msg.command == "unstage" and msg.obj.name == "fastcs-eiger"
         ),
     )
+
+
+def test_take_eiger_and_i0_data_makes_expected_calls(eiger: EigerDetector):
+    run_engine = RunEngineSimulator()
+    msgs = run_engine.simulate_plan(take_eiger_data(10, 0.1, eiger))
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: (
+            msg.command == "unstage" and msg.obj.name == "fastcs-eiger"
+        ),
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "unstage" and msg.obj.name == "i0",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "stage" and msg.obj.name == "fastcs-eiger",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "stage" and msg.obj.name == "i0",
+    )
+
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "wait",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "open_run",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: (
+            msg.command == "prepare" and msg.obj.name == "fastcs-eiger"
+        ),
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "prepare" and msg.obj.name == "i0",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "declare_stream",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: (
+            msg.command == "kickoff" and msg.obj.name == "fastcs-eiger"
+        ),
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "kickoff" and msg.obj.name == "i0",
+    )
+
+    for _ in range(10):
+        msgs = assert_message_and_return_remaining(
+            msgs,
+            predicate=lambda msg: (
+                msg.command == "trigger"
+                and msg.obj.name == "fastcs-eiger-detector-trigger"
+            ),
+        )
+        msgs = assert_message_and_return_remaining(
+            msgs,
+            predicate=lambda msg: (
+                msg.command == "set"
+                and msg.obj.name == "zebra-inputs-soft_in_1"
+                and msg.args[0] == 1
+            ),
+        )
+
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: (
+            msg.command == "complete" and msg.obj.name == "fastcs-eiger"
+        ),
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "complete" and msg.obj.name == "i0",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "collect",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "close_run",
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: (
+            msg.command == "unstage" and msg.obj.name == "fastcs-eiger"
+        ),
+    )
+    msgs = assert_message_and_return_remaining(
+        msgs,
+        predicate=lambda msg: msg.command == "unstage" and msg.obj.name == "i0",
+    )
