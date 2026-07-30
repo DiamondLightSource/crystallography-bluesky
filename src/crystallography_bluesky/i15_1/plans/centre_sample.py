1b15ba4f893c87c63c2b1596bed14eace779399b
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-07-30 10:01:16 +0100

    Remove log

--- src/crystallography_bluesky/i15_1/plans/centre_sample.py
+++ src/crystallography_bluesky/i15_1/plans/centre_sample.py
@@ -79,7 +79,6 @@ def centre_sample(
     )
 
     analysis_result = analysis_callback.wait_on_and_retrieve_result()
-    LOGGER.info(f"analysis result: {analysis_result}")
     centre = analysis_result["centre"]
     assert (start_z < centre < end_z) or (start_z > centre > end_z), (
         f"Analysis result {centre} is not within the bounds of the scan: "
