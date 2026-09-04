3eaed76864e0ec91eea32a7bac2e808542aeccd7
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-09-04 13:17:58 +0100

    Added attenuation reading as baseline reading

--- src/crystallography_bluesky/i15_1/plans/generic_collection.py
+++ src/crystallography_bluesky/i15_1/plans/generic_collection.py
@@ -6,6 +6,7 @@ import bluesky.preprocessors as bpp
 import pydantic
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
+from dodal.devices.beamlines.i15_1.attenuator import Attenuator
 from dodal.devices.beamlines.i15_1.laue import LaueMonochrometer
 from dodal.devices.beamlines.i15_1.robot import Robot
 from dodal.devices.tetramm import SummingTetrammDetector
@@ -32,6 +33,7 @@ class GenericCollectionDevices:
     tth: Motor
     fast_shutter: ZebraFastShutter
     xtal: LaueMonochrometer
+    attenuator: Attenuator
 
 
 def setup_and_teardown_collection(
@@ -161,7 +163,12 @@ def generic_per_step_collection(
             yield from bps.abs_set(devices.zebra.inputs.soft_in_1, 0, wait=True)
             yield from per_step()
 
-    DEFAULT_BASELINE_DEVICES = [devices.robot.spinner, devices.xtal, devices.tth]
+    DEFAULT_BASELINE_DEVICES = [
+        devices.robot.spinner,
+        devices.xtal,
+        devices.tth,
+        devices.attenuator,
+    ]
 
     yield from setup_and_teardown_collection(
         frames=frames,
