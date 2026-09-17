db63461c6c27848716ad0285fd17db2e983911b5
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-09-17 10:28:03 +0100

    Fix bug and add test (#152)

--- tests/unit_tests/i15_1/test_generic_collection.py
+++ tests/unit_tests/i15_1/test_generic_collection.py
@@ -1,5 +1,7 @@
+from typing import Any
 from unittest.mock import MagicMock, patch
 
+import pytest
 from bluesky import RunEngine
 
 from crystallography_bluesky.i15_1.plans.generic_collection import (
@@ -8,7 +10,9 @@ from crystallography_bluesky.i15_1.plans.generic_collection import (
 )
 
 
+@pytest.mark.parametrize("sample_metadata", [{"c": "d"}, None])
 def test_setup_and_tear_down_collection_changes_sample_md_key_and_adds_detectors(
+    sample_metadata: dict[str, Any] | None,
     run_engine: RunEngine,
     common_collection_devices: GenericCollectionDevices,
 ):
@@ -21,14 +25,17 @@ def test_setup_and_tear_down_collection_changes_sample_md_key_and_adds_detectors
                 exposure_time=0.01,
                 devices=common_collection_devices,
                 collection=MagicMock(),
-                metadata={"experiment_definition": {"a": "b"}, "sample": {"c": "d"}},
+                metadata={
+                    "experiment_definition": {"a": "b"},
+                    "sample": sample_metadata,
+                },
             )
         )
 
     mock_run_decorator.assert_called_once_with(
         md={
             "experiment_definition": {"a": "b"},
-            "sample_info": {"c": "d"},
+            "sample_info": sample_metadata,
             "detectors": ["fastcs-eiger", "i0"],
         }
     )
